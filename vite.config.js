73dab220cdcf296cbc84659fb37d55711fc2d5e2
Nauval Fahreza A <nauvalfahreza03@gmail.com>
2023-03-31 14:34:31 +0700

    remove google verif

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,6 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  build: {
-    rollupOptions: {
-      input: {
-        main: './index.html',
-        google: './gle077a68ad4512ff47.html' // Nama file HTML dari Google
-      }
-    }
-  },
   base: '/',
   plugins: [react()]
 })
